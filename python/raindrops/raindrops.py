06b4695e9f112b823815d89d135b03bac0a63b7e
HyoSung Bidol-Lee <hyosung11@me.com>
2020-01-29 11:32:24 -0500

    submit solution to exercism.io

---
+++ python/raindrops/raindrops.py
@@ -0,0 +1,23 @@
+# Exercism
+# Python Track
+# Exercise 2 - Raindrops
+
+def convert(number):
+    result = ''
+
+    if number % 3 == 0:
+        result += 'Pling'
+
+    if number % 5 == 0:
+        result += 'Plang'
+
+    if number % 7 == 0:
+        result += 'Plong'
+
+    # does not have any of 3, 5, or 7 as a factor, the result should be the digits of the number.
+    if not result:
+        result = number
+
+    return str(result)
+
+print(convert(5005))
