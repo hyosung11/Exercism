61bc975aee451e342ecbe0265814e9dbacd6a2cb
HyoSung Bidol-Lee <hyosung11@me.com>
2020-02-04 17:58:24 -0500

    commit after save

--- csharp/two-fer/TwoFer.cs
+++ csharp/two-fer/TwoFer.cs
@@ -2,7 +2,7 @@
 
 public static class TwoFer
 {
-    // In order to get the tests running, first you need to make sure the Speak method 
+    // In order to get the tests running, first you need to make sure the Speak method
     // can be called both without any arguments and also by passing one string argument.
     public static string Speak(string name = "you")
     {
